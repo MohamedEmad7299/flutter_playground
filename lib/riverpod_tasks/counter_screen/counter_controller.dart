e66cce08e604579f6d3b8297ae3500615a297c9b
Mohamed Emad <mohamedemad199912@gmail.com>
2025-08-20 10:42:15 +0300

    just rewrite it again to know the syntax more

--- lib/riverpod_tasks/counter_screen/counter_controller.dart
+++ lib/riverpod_tasks/counter_screen/counter_controller.dart
@@ -1,15 +1,13 @@
 
-
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterController extends StateNotifier<int>{
+class CounterNotifier extends StateNotifier<int>{
 
-  CounterController() : super(0);
+  CounterNotifier() : super(0);
 
   void increment() => state++;
   void reset() => state = 0;
   void decrement() => state--;
 }
 
-final counterNotifierProvider = StateNotifierProvider<CounterController,int>((ref) => CounterController());
+final counterNotifierProvider = StateNotifierProvider((ref) => CounterNotifier());
